cf02bd8c04b5b115e32e550c54c9e99e5b7a449e
David-areiza <davidestibencor1@gmail.com>
2023-08-01 21:18:38 -0500

    cllas 5

---
+++ index.js
@@ -0,0 +1,15 @@
+let A = 10;
+var D = 23;
+const C  = 58; 
+
+console.log({ A });
+console.log({ D });
+console.log({ C });
+
+console.table({A,D,C});
+
+
+let e = ' hola ',
+q= 'spiderman '
+
+console.log(e + q)
---
+++ tarea1.js
@@ -0,0 +1,10 @@
+
+let = nombre = 'David estiven';
+let = apellido = 'correa areiza';
+let  = ano =  '2003';
+let = ciudad = 'medellin' ;
+let = edad = ' 19 years  '
+
+
+console.log('hello my name is , ', nombre , apellido , 'and i live in', ciudad ,'and i am', edad, '.' )
+
